205c9a51288e6a4e5e6ef46fcd98fc4913e8b2c3
PersesTitan <torampersesTitan@gmail.com>
2023-04-11 12:37:51 +0900

    change readFile param name

--- src/com/badword/method/ReadFile.java
+++ src/com/badword/method/ReadFile.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public interface ReadFile extends EditWord {
-    default void addFile(File file, String criteria, boolean rmBlank) {
+    default void addFile(File file, String delim, boolean rmBlank) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             StringBuilder fileContent = new StringBuilder();
             reader.lines().forEach(fileContent::append);
-            StringTokenizer tokenizer = criteria == null
+            StringTokenizer tokenizer = delim == null
                     ? new StringTokenizer(fileContent.toString())
-                    : new StringTokenizer(fileContent.toString(), criteria);
+                    : new StringTokenizer(fileContent.toString(), delim);
             if (rmBlank) {
                 while (tokenizer.hasMoreTokens()) add(tokenizer.nextToken().strip());
             } else {
@@ -28,24 +28,24 @@ public interface ReadFile extends EditWord {
         addFile(path, null, true);
     }
 
-    default void addFile(String path, String criteria) {
-        addFile(path, criteria, true);
+    default void addFile(String path, String delim) {
+        addFile(path, delim, true);
     }
 
     default void addFile(String path, boolean rmBlank) {
         addFile(path, null, rmBlank);
     }
 
-    default void addFile(String path, String criteria, boolean rmBlank) {
-        addFile(new File(path), criteria, rmBlank);
+    default void addFile(String path, String delim, boolean rmBlank) {
+        addFile(new File(path), delim, rmBlank);
     }
 
     default void addFile(File file) {
         addFile(file, null, true);
     }
 
-    default void addFile(File file, String criteria) {
-        addFile(file, criteria, true);
+    default void addFile(File file, String delim) {
+        addFile(file, delim, true);
     }
 
     default void addFile(File file, boolean rmBlank) {
