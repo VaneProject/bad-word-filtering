823c52ddb335d87707d009800b26eec1e33416da
PersesTitan <torampersesTitan@gmail.com>
2023-04-11 13:25:10 +0900

    change public

--- src/com/badword/method/EditWord.java
+++ src/com/badword/method/EditWord.java
@@ -3,7 +3,7 @@ package com.badword.method;
 import java.util.List;
 import java.util.Set;
 
-public interface EditWord {
+interface EditWord {
     void add(String text);
     void add(String...texts);
     void add(List<String> texts);
